2f3cc712cc35eb32da4f5357f19a862cb09ef4d3
Melinacaesarw01 <54980329+Melinacaesarw01@users.noreply.github.com>
2020-12-25 19:16:52 +0700

    admin tambah peminjaman

--- resources/views/ui/dashboard/modules/admin/peminjamans/create.blade.php
+++ resources/views/ui/dashboard/modules/admin/peminjamans/create.blade.php
@@ -22,8 +22,13 @@
         <div class="col-lg-6">
             <div class="card">
                 <div class="card-body">
+<<<<<<< Updated upstream
                      <div class="form-group">
                         <label for="">Buku Yang Dipinjam</label>
+=======
+                    <div class="form-group">
+                        <label for="">Nama Buku</label>
+>>>>>>> Stashed changes
                         <select name="" id="" class="selectpicker" data-style="form-control" data-width="100%" data-live-search="true">
                             <option value="">Buku Laravel</option>
                         </select>
@@ -35,12 +40,21 @@
                         </select>
                     </div>
                     <div class="form-group">
+<<<<<<< Updated upstream
                         <label for="">Tanggal Peminjaman</label>
                         <input data-input="datetimepicker" type="text" class="form-control" name="name" autocomplete="off">
                     </div>
                     <div class="form-group">
                         <label for="">Tanggal Kembali</label>
                         <input data-input="datetimepicker" type="text" class="form-control" name="name" autocomplete="off">
+=======
+                        <label class="control-label" for="date">Tanggal Peminjaman</label>
+                        <input class="form-control" id="date" name="date"  type="date"/>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label" for="date">Tanggal Pengembalian</label>
+                        <input class="form-control" id="date" name="date"  type="date"/>
+>>>>>>> Stashed changes
                     </div>
                     <button class="btn btn-lg btn-primary ml-auto d-block">Tambahkan</button>                    
                 </div>
@@ -48,16 +62,6 @@
         </div>
         <div class="col-lg-6 col-xl-4 col-xl-auto">
             <div class="row">
-                <div class="col-md-6 col-lg-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <h6>Foto Member</h6>
-                            <div id="img-card">
-                                <img src="" class="img-fluid" id="img-user">
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <div class="col-md-6 col-lg-12">
                     <div class="card">
                         <div class="card-body">
