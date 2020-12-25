5f13fd24df2ba82a843831c13f85f18ea3002084
febriaerliana <febriaerliana16@gmail.com>
2020-12-25 16:28:35 +0700

    update widget dashboard

--- resources/views/ui/dashboard/modules/member/index.blade.php
+++ resources/views/ui/dashboard/modules/member/index.blade.php
@@ -2,79 +2,65 @@
 
 @section('title', 'Dashboard')
 
-@section('header', 'Pengembalian Buku Kamu')
+@section('header', 'Selamat Datang, di Perpus ID!')
 @section('breadcrumb')
-    <div class="breadcrumb-item active"><a href="#">Pengembalian</a></div>
+    <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
     {{-- <div class="breadcrumb-item">Activities</div> --}}
 @endsection
 @section('content-header')
   <div class="row align-items-center">
-		<div class="col-md"><h2 class="section-title">Daftar Pengembalian</h2></div>
+		<div class="col-md"><h2 class="section-title">Dashboard</h2></div>
   </div>
 @endsection
 
 
 @section('content')
-<div class="row">
-            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-              <div class="card card-statistic-1">
-                <div class="card-icon bg-primary">
-                  <i class="far fa-user"></i>
+
+    {{-- widget --}}
+    <div class="row">
+                <div class="col-md">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-primary">
+                    <i class="far fa-newspaper"></i>
+                    </div>
+                    <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Total Buku Dipinjam</h4>
+                    </div>
+                    <div class="card-body">
+                        10
+                    </div>
+                    </div>
                 </div>
-                <div class="card-wrap">
-                  <div class="card-header">
-                    <h4>Total Admin</h4>
-                  </div>
-                  <div class="card-body">
-                    10
-                  </div>
                 </div>
-              </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-              <div class="card card-statistic-1">
-                <div class="card-icon bg-danger">
-                  <i class="far fa-newspaper"></i>
+                <div class="col-md">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-success">
+                    <i class="fas fa-shopping-basket"></i>
+                    </div>
+                    <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Total Buku Dikembalikan</h4>
+                    </div>
+                    <div class="card-body">
+                        42
+                    </div>
+                    </div>
                 </div>
-                <div class="card-wrap">
-                  <div class="card-header">
-                    <h4>News</h4>
-                  </div>
-                  <div class="card-body">
-                    42
-                  </div>
                 </div>
-              </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-              <div class="card card-statistic-1">
-                <div class="card-icon bg-warning">
-                  <i class="far fa-file"></i>
+                <div class="col-md">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-danger">
+                    <i class="fas fa-exclamation-circle"></i>
+                    </div>
+                    <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Total Buku Terlambat</h4>
+                    </div>
+                    <div class="card-body">
+                        1,201
+                    </div>
+                    </div>
                 </div>
-                <div class="card-wrap">
-                  <div class="card-header">
-                    <h4>Reports</h4>
-                  </div>
-                  <div class="card-body">
-                    1,201
-                  </div>
                 </div>
-              </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-              <div class="card card-statistic-1">
-                <div class="card-icon bg-success">
-                  <i class="fas fa-circle"></i>
-                </div>
-                <div class="card-wrap">
-                  <div class="card-header">
-                    <h4>Online Users</h4>
-                  </div>
-                  <div class="card-body">
-                    47
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
 @endsection
