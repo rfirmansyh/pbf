cea5d9220830384e8f4aa446cf2d08ea1ed008ce
febriaerliana <febriaerliana16@gmail.com>
2020-12-26 16:34:32 +0700

    update href nya admin show books

--- resources/views/ui/dashboard/modules/admin/books/show.blade.php
+++ resources/views/ui/dashboard/modules/admin/books/show.blade.php
@@ -11,7 +11,7 @@
   <div class="row align-items-center">
         <div class="col-md"><h2 class="section-title">Baca Detail Belajar Laravel</h2></div>
         <div class="col-md-auto">
-            <a href="{{ url('ui/dashboard/member/books/') }}" class="btn btn-block btn-lg btn-outline-secondary"><i class="fas fa-arrow-left mr-2"></i> Kembali</a>
+            <a href="{{ url('ui/dashboard/admin/books/') }}" class="btn btn-block btn-lg btn-outline-secondary"><i class="fas fa-arrow-left mr-2"></i> Kembali</a>
         </div>
   </div>
 @endsection
